9c7483bccd466f727e51c8c741737d48c65e53d8
gembleman <81058727+gembleman@users.noreply.github.com>
2022-12-16 03:16:56 +0900

    Update and rename chr&cha열_추출.py to tsv_chr_cha열_추출.py

--- tsv_chr_cha열_추출.py
+++ tsv_chr_cha열_추출.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 path_dir = 'tsv_shift_jis'
-save_dir = 'chr&cha열_추출'
+save_dir = 'tsv_chr_cha열_추출'
 file_list = []
 
 for path, dirs, files in os.walk(path_dir):
@@ -12,7 +12,7 @@ for path, dirs, files in os.walk(path_dir):
 
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
-    print('chr&cha열_추출 폴더 생성 완료~')
+    print('tsv_chr_cha열_추출 폴더 생성 완료~')
 
 for l in file_list:
     df = pd.read_csv(l, sep = '\t', encoding='shift-jis',na_values =['N\A'],encoding_errors="replace")
@@ -29,5 +29,5 @@ for l in file_list:
     if sin == 1:
         l2 = l.split('\\')
         xx.to_csv(save_dir+'//'+l2[-1]+"_추출.txt", sep='$', index=False ,header = None, encoding='utf-8')
-print('chr&cha열_추출 완료~ ^^')
+print('tsv_chr_cha열_추출 완료~ ^^')
 os.system('pause')
