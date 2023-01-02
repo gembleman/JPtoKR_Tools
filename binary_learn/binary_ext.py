8d37645ec36f4651a16f962537db1ce3dfd56323
gembleman <81058727+gembleman@users.noreply.github.com>
2023-01-03 03:35:49 +0900

    Update binary_ext.py

--- binary_learn/binary_ext.py
+++ binary_learn/binary_ext.py
@@ -1,11 +1,12 @@
 import os
 
-path_dir = "추출할dat"
-save_dir = "dat저장폴더"
+path_dir = "원본dat"
+save_dir = "추출완료"
 file_list = os.listdir(path_dir)
 
 
 for li in file_list:
+    print(li)
     a = open(path_dir + "//" + li, "rb")  # 불러올 바이너리 파일
     a1 = bytearray(a.read())
     a.close()
@@ -51,40 +52,34 @@ for li in file_list:
     for aaa in cola1:
 
         a34 = hex(aaa)
-        # print(a34)
         if a34 not in dic:
             flag = None
             num2 = num
             while True:
                 num2 += 1  # 다음 헥스값을 호출하기 위해
                 a45 = hex(a1[num2])
-                if a45 not in dic:
-                    flag = 2
-                else:  # 다음에 0이나 FF가 오는 경우
-                    if num == num2 - 1:  # 바로 다음에 0이나FF가 온다는 소리니까. 그런 건 걸러낸다.
-                        flag = 1
+                if a45 in dic:
                     break
 
-            if flag == 2:
-                # print("num:" + str(num) + "num2:" + str(num2))
-                b2 = a1[num:num2]
-                # print(b2)
-
-                ext_list.append(b2)
-                num3 = num2 - num
-                # print("이만큼 커서를 건너뜁니다 : " + str(num3))
-                for x in range(num3):
-                    num += 1
-                    next(cola1)
-                    # 이게 핵심. - 리스트에서 추출한 요소들을 반복문으로 불러오지 않고 건너뛰게 한다.
-                    # 예를 들어 리스트에서 \xf0을 감지해 \xf0가 어디까지 반복되고 있는지 계산하고 추출하는 반복문이 있다면
-                    # 반복문이 한 번 추출했으면 \xf0 다음에 오는 또다른 \xf0을 불러오지 않고 건너뛰게 하는 방법이 필요하다.
-
-            if flag == 1:
+            if len(a1[num:num2]) == 1:  # 바이트 하나 크기로 걸러진 건 추출하지 않음.
                 num += 1
-                continue  # 다음으로 넘어감.
+                continue
 
-        num += 1
+            b2 = a1[num:num2]
+            ext_list.append(b2)
+            for x in range(num2 - num - 1):  # 하나 더 빼줘야 커서 위치가 정확하게 옮겨짐.
+                num += 1
+                next(cola1)
+                # 이게 핵심. - 리스트에서 추출한 요소들을 반복문으로 불러오지 않고 건너뛰게 한다.
+                # 예를 들어 리스트에서 \xf0을 감지해 \xf0가 어디까지 반복되고 있는지 계산하고 추출하는 반복문이 있다면
+                # 반복문이 한 번 추출했으면 \xf0 다음에 오는 또다른 \xf0을 불러오지 않고 건너뛰게 하는 방법이 필요하다.
+
+            num += 1
+            continue
+
+        else:
+            num += 1
+
     file_name = save_dir + "//" + li + ".txt"
     file_name2 = save_dir + "//" + li + "_hex.txt"
     jj = open(file_name, "w", encoding="utf-8")
