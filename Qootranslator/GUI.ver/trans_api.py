e5d57a14e02ff327003f63795937b9f1f6d710fb
gembleman <nsoop67@gmail.com>
2023-04-28 06:30:31 +0900

    fix

--- Qootranslator/GUI.ver/trans_api.py
+++ Qootranslator/GUI.ver/trans_api.py
@@ -8,16 +8,16 @@ class Trans_api:
     def __init__(self, setting):
         self.papago_free_client_id = setting["api_keys"]["papago_free_api"]  # 개발자센터에서 발급받은 Client ID 값
         self.papago_free_client_secret = setting["api_keys"]["papago_free_api"]  # 개발자센터에서 발급받은 Client Secret 값
+        self.source_lang = setting["source_lang"]
+        self.target_lang = setting["target_lang"]
 
     def papago_free_api(self):
-        client_id = "YOUR_CLIENT_ID"  # 개발자센터에서 발급받은 Client ID 값
-        client_secret = "YOUR_CLIENT_SECRET"  # 개발자센터에서 발급받은 Client Secret 값
         encText = urllib.parse.quote("반갑습니다")
-        data = "source=ko&target=en&text=" + encText
+        data = f"source={self.source_lang}&target={self.target_lang}&text=" + encText
         url = "https://openapi.naver.com/v1/papago/n2mt"
         request = urllib.request.Request(url)
         request.add_header("X-Naver-Client-Id", self.papago_free_client_id)
-        request.add_header("X-Naver-Client-Secret", client_secret)
+        request.add_header("X-Naver-Client-Secret", self.papago_free_client_secret)
         response = urllib.request.urlopen(request, data=data.encode("utf-8"))
         rescode = response.getcode()
         if rescode == 200:
@@ -25,3 +25,7 @@ class Trans_api:
             print(response_body.decode("utf-8"))
         else:
             print("Error Code:" + rescode)
+
+
+# todo
+# 파일 번역할 때, 특정 문자가 들어가있으면 그 줄은 번역하지 않도록 하는 설정.
